cea1783a6fa88930e9916119e6503c0d9fe0172c
P2541536 <119421978+P2541536@users.noreply.github.com>
2023-04-18 17:16:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './from.css';
+import From from './from';
+function App() {
+  return (
+    <div className="App">
+<From/>
+    </div>
+  );
+}
+
+export default App;
